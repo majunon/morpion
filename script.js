88b1796d9b61d70d4ebb745df968ecfbba9d2e82
majunon <fayardjul@gmail.com>
2021-09-28 12:55:49 +0200

    add getZone and move board to private scope

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 // The Gameboard
 const Game = (() => {
-  let board = new Array(9);
-  return ({ board });
+  // let _board = new Array(3);
+  // for (let i=0; i< _board.length; i++){
+  //   _board[i]= new Array(3);
+  // }
+
+  let _board = [[1,2,3],[4,5,6],[7,8,9]];
+
+  const _wrapper = document.querySelector('#wrapper');
+
+  const render = () => {
+    for(let i=0; i<3 ; i++){
+      let _col = document.createElement('div');
+      for(let j=0; j<3; j++){
+        let zone = document.createElement('button');
+        zone.textContent = _board[i][j];
+        _col.appendChild(zone);
+      }
+      _wrapper.appendChild(_col);
+    }
+  }
+
+  const getZone = (position) => {
+    return(_board[Math.trunc(position/3)][position%3]);
+  }
+
+
+  return { render, getZone };
 })();
 
 // The players
@@ -13,3 +38,14 @@ const Player = (name, shape) => {
 
 const player1 = Player('Ju','X');
 const player2 = Player('Manon','O');
+
+// The game
+const Partie = (() => {
+  
+})();
+
+
+
+// FOR TESTING PURPOSE
+
+Game.render();
